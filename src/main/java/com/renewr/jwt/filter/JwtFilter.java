90f3e4e8dd2798c871689a988ac5ed7ab5e552f7
heo__ony <oueya1479@gmail.com>
2023-12-12 09:09:56 +0900

    Feat: JWT 및 Security 기능 구현

--- src/main/java/com/renewr/jwt/filter/JwtFilter.java
+++ src/main/java/com/renewr/jwt/filter/JwtFilter.java
@@ -1,23 +1,31 @@
 package com.renewr.jwt.filter;
 
+import com.renewr.jwt.entity.Token;
 import com.renewr.jwt.provider.TokenProvider;
+import com.renewr.jwt.repository.TokenRepository;
+import com.renewr.member.domain.Member;
+import com.renewr.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);
 
@@ -25,21 +33,29 @@ public class JwtFilter extends GenericFilterBean {
 
     private final TokenProvider tokenProvider;
 
+    private final TokenRepository tokenRepository;
+
+    private final MemberRepository memberRepository;
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String jwt = resolveToken(httpServletRequest);
-        String requestURI = httpServletRequest.getRequestURI();
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = resolveToken(request);
+        String requestURI = (request).getRequestURI();
 
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-            Authentication authentication = tokenProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            logger.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            Token token = tokenRepository.findByAccessToken(jwt).orElseThrow(() -> new RuntimeException("토큰이 존재하지 않습니다."));
+            Member member = memberRepository.findById(token.getMemId()).orElseThrow(() -> new RuntimeException("멤버가 존재하지 않습니다."));
+            if (member != null) {
+                Authentication authentication = tokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                throw new RuntimeException("멤버가 존재하지 않습니다.");
+            }
         } else {
             logger.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
         }
 
-        chain.doFilter(request, response);
+        filterChain.doFilter(request, response);
     }
 
     private String resolveToken(HttpServletRequest request) {
